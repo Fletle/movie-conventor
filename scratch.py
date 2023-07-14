7fd947e2df719d6bd1ad167c399605cfc7e06b9d
Fletle <terminatrt@gmail.com>
2023-07-14 23:00:11 +0300

    Programm

--- scratch.py
+++ scratch.py
@@ -1,2 +1,8 @@
 import moviepy.editor
-video = moviepy.editor.VideoFileClip(input('Введите путь до видео файла'))
+
+path = input('Введите путь до папки, где лежит видеофайл') + '\\'
+name = input('Введите название видеофайла (с расширением)')
+video = moviepy.editor.VideoFileClip(path + name)
+audio = video.audio
+name = name.split('.').pop(0)  # получение названия файла
+audio.write_audiofile(f'{path}\\{name}.mp3')
